25b9f52b425fc3ac561a7e30b4133aede084014f
jmonto55 <jmonto55@gmail.com>
2023-02-20 14:16:31 -0500

    Minor styles changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from '../sass/Header.module.scss';
 const Header = () => (
   <header style={styles} className={styles.header}>
     <h1>todos</h1>
-    <p>Items will persist ini the browser local storage</p>
+    <p>Items will persist in the browsers local storage</p>
   </header>
 );
 export default Header;
