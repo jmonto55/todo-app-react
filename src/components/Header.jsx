5a3cdee9554782a98392f63b07e636e76cb1171c
jmonto55 <jmonto55@gmail.com>
2023-02-23 17:15:28 -0500

    Fix styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import styles from '../sass/Header.module.scss';
 
 const Header = () => (
-  <header style={styles} className={styles.header}>
+  <header className={styles.header}>
     <h1>todos</h1>
     <p>Items will persist in the browsers local storage</p>
   </header>
